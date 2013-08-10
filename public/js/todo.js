bc61b17f3f6789e9ddb73f7fcf5f53241f258cc8
OpenShift guest <andrew.beng.2010@sis.smu.edu.sg>
2013-08-10 02:34:29 -0400

    added Procfile

--- public/js/todo.js
+++ public/js/todo.js
@@ -2,34 +2,51 @@ var todoApp = angular.module('todoApp', []);
 
 todoApp.controller('TodoController', function($scope, $http) {
 
-  $scope.todos = [];
+    $scope.todos = [];
 
-  $scope.addTodo = function(title) {
-    $scope.newTodoTitle = '';
-    $scope.todos.push({
-        title: title,
-        completed: false
-    });
-  };
+    $scope.addTodo = function(title) {
+        $scope.newTodoTitle = '';
+        $scope.todos.push({
+            title: title,
+            completed: false,
+            id: generateID()
+        });
+    };
 
-  $scope.changeCompleted = function(todo) {
-    // Update the todo
-    var message = (todo.completed === true) ? 'Task Completed!' : 'Task Uncompleted!';
-  };
+    $scope.changeCompleted = function(todo) {
+        // Update the todo
+        var message = (todo.completed === true) ? 'Task Completed!' : 'Task Uncompleted!';
+    };
 
-  $scope.removeCompletedItems = function() {
-    $scope.todos.forEach(function(todo) {
-        if (todo.completed === true) {
-            deleteTodo(todo);
-        }
-    });
-  };
+    $scope.removeCompletedItems = function() {
+        var uncompleted_todos = [];
+        $scope.todos.forEach(function(todo) {
+            if (todo.completed === true) {
+                deleteTodo(todo.id);
+            }
+            else {
+                uncompleted_todos.push(todo);
+            }
+        });
+        $scope.todos = uncompleted_todos;
+    };
 
-  function deleteTodo(todo) {
-      if ($scope.todos.indexOf(todo) !== -1) {
-        var index = $scope.todos.indexOf(todo);
-        $scope.todos.splice(index, 1);
-      }
-  }
+    function deleteTodo(id) {
+        /*$scope.todos.forEach(function(todo, i) {
+          if (todo.id === id) {
+              return $scope.todos.splice(i, 1);
+          }
+      });*/
+    }
+
+    function generateID() {
+        var rand = Math.random();
+        $scope.todos.forEach(function(todo) {
+            if (todo.id === rand) {
+                generateID();
+            }
+        });
+        return rand;
+    }
 
 });
