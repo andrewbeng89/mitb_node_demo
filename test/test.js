a4dce8be1a6075fa8a2e4cc958d95a6d1642efe1
andrewbeng89 <andrew.beng.2010@sis.smu.edu.sg>
2013-08-13 02:09:57 +0000

    fixed build error

--- test/test.js
+++ test/test.js
@@ -1,13 +1,9 @@
 var app = require('../app'), http = require('http'), request = require('supertest'), assert = require('assert');
 
-describe('Test Express App', function() {
-	it('should GET /', function(done) {
-		request(app)
-			.get('/index.html')
-			.expect(200)
-			.end(function(err, res){
-        		if (err) return done(err);
-        		done();
-      		});
-	});
+describe('GET /index.html', function(){
+  it('get index.html', function(done){
+    request(app)
+      .get('/index.html')
+      .expect(200, done);
+  });
 });
