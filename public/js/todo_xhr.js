240f3e5411f7082ced76e6f8633cb12bd35a0d97
andrewbeng89 <andrew.beng.2010@sis.smu.edu.sg>
2013-08-13 02:10:43 +0000

    drone.io update

--- public/js/todo_xhr.js
+++ public/js/todo_xhr.js
@@ -2,88 +2,72 @@ var todoApp = angular.module('todoApp', []);
 
 todoApp.controller('TodoController', function($scope, $http) {
 
-  $scope.todos = [];
-
-  // Get all todos
-  /*$http.get('/todos')
+    $scope.todos = [];
+    $scope.loaded = false;
+    
+    $http.get('/todos')
     .success(function(todos) {
-      $scope.loaded = true;
-      $scope.todos = todos;
+      
     }).error(function(err) {
       alert(err);
-    });*/
-
-  $scope.addTodo = function(title) {
-    /*$http.post('/todos', {
-      title: title
-    }).success(function(todo) {
-      $scope.newTodoTitle = '';
-      $scope.todos.push(todo);
-    }).error(function(err) {
-      // Alert if there's an error
-      return alert(err.message || "an error occurred");
-    });*/
-    $scope.newTodoTitle = '';
-    $scope.todos.push({
-        title: title,
-        completed: false
     });
-  };
-
-  $scope.changeCompleted = function(todo) {
-    // Update the todo
-    var message = (todo.completed === true) ? 'Task Completed!' : 'Task Uncompleted!';
-    /*$http.put('/todos/' + todo.id, {
-      completed: todo.completed
-    }).error(function(err) {
-      return alert(err.message || (err.errors && err.errors.completed) || "an error occurred");
-    });*/
-  };
-
-  $scope.removeCompletedItems = function() {
-    /*$http.get('/todos', {
-      params: {
-        completed: true
-      }
-    }).success(function(todos) {
-      todos.forEach(function(t) { deleteTodo(t); });
-    });*/
-    $scope.todos.forEach(function(todo) {
-        if (todo.completed === true) {
-            deleteTodo(todo);
-        }
-    });
-  };
-
-  function deleteTodo(todo) {
-    // Find the index of an object with a matching id
-      /*var index = $scope.todos.indexOf(
-          $scope.todos.filter(function(t) {
-            return t.id === todo.id;
-          })[0]);*/
-
-      if ($scope.todos.indexOf(todo) !== -1) {
-        var index = $scope.todos.indexOf(todo);
-        $scope.todos.splice(index, 1);
-      }
     
-    /*$http.delete('/todos/' + todo.id, {
-      params: {
-        completed: true
-      }
-    }).success(function() {
-      // Find the index of an object with a matching id
-      var index = $scope.todos.indexOf(
-          $scope.todos.filter(function(t) {
-            return t.id === todo.id;
-          })[0]);
+    $http.get('/todos').success(function(data, status, headers, config) {
+        $scope.loaded = true;
+        $scope.todos = data;
+    });
 
-      if (index !== -1) {
-        $scope.todos.splice(index, 1);
-      }
-    }).error(function(err) {
-      alert(err.message || "an error occurred");
-    });*/
-  }
+    $scope.addTodo = function(title) {
+        if (title) {
+            var new_todo = {
+                title: title,
+                completed: false,
+                id: generateID()
+            };
+            $http.post('/todos', new_todo).success(function(data, status, headers, config) {
+                $scope.newTodoTitle = '';
+                $scope.todos.push(new_todo);
+            });
+        }
+    };
+
+    $scope.changeCompleted = function(todo) {
+        // Update the todo
+        var message = (todo.completed === true) ? 'Task Completed!' : 'Task Uncompleted!';
+    };
+
+    $scope.removeCompletedItems = function() {
+        var uncompleted_todos = [];
+        $scope.todos.forEach(function(todo) {
+            if (todo.completed === true) {
+                console.log(todo.id);
+                deleteTodo(todo.id);
+            }
+            else {
+                uncompleted_todos.push(todo);
+            }
+        });
+        $scope.todos = uncompleted_todos;
+    };
+
+    function deleteTodo(id) {
+        $http.delete('/todos/' + id).success(function(data, status, headers, config) {
+            if (data.result === 'success') {
+                console.log('Todo deleted! ID: ' + id);
+            } else {
+                console.log('Something went wrong: ' + data.error);
+            }
+        });
+    }
+
+    function generateID() {
+        var rand = Math.random();
+        $scope.todos.forEach(function(todo) {
+            if (todo.id === rand) {
+                generateID();
+            }
+        });
+        return rand;
+    }
 
 });
