b7ae31537e559f587aac51baf7eafaef9f1b409d
andrewbeng89 <andrew.beng.2010@sis.smu.edu.sg>
2013-08-27 06:59:21 +0000

    travis-ci update

--- public/js/todo.js
+++ public/js/todo.js
@@ -2,10 +2,14 @@ var todoApp = angular.module('todoApp', []);
 
 todoApp.controller('TodoController', function($scope, $http) {
 
+    // Array representing the list of todos
     $scope.todos = [];
 
     $scope.addTodo = function(title) {
+        // Reset the title to an empty string
         $scope.newTodoTitle = '';
+        // Each todo is an object with a title, completed status and a generated ID
+        // Push the newly created todo into the list
         $scope.todos.push({
             title: title,
             completed: false,
@@ -19,7 +23,9 @@ todoApp.controller('TodoController', function($scope, $http) {
     };
 
     $scope.removeCompletedItems = function() {
+        // Array to contain all uncompleted todos
         var uncompleted_todos = [];
+        // If a todo is completed, delete it
         $scope.todos.forEach(function(todo) {
             if (todo.completed === true) {
                 deleteTodo(todo.id);
@@ -28,17 +34,15 @@ todoApp.controller('TodoController', function($scope, $http) {
                 uncompleted_todos.push(todo);
             }
         });
+        // Set todo list to list of uncompleted todos
         $scope.todos = uncompleted_todos;
     };
 
     function deleteTodo(id) {
-        /*$scope.todos.forEach(function(todo, i) {
-          if (todo.id === id) {
-              return $scope.todos.splice(i, 1);
-          }
-      });*/
+        // Delete function only for todo_xhr.js
     }
 
+    // Random ID generator
     function generateID() {
         var rand = Math.random();
         $scope.todos.forEach(function(todo) {
