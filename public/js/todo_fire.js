51df0cf78ad3dcb15998027f95c20ce44fcd7ac4
andrewbeng89 <andrew.beng.2010@sis.smu.edu.sg>
2013-09-06 19:24:10 +0000

    travis-ci update

---
+++ public/js/todo_fire.js
@@ -0,0 +1,61 @@
+var todoApp = angular.module('todoApp', ['firebase']);
+
+todoApp.controller('TodoController', ['$scope', 'angularFire',
+    function($scope, angularFire) {
+        // Array representing the list of todos
+        $scope.todos = [];
+        // Load status
+        $scope.loaded = false;
+        
+        var ref = new Firebase('https://mitb-demo.firebaseio.com/todos');
+        angularFire(ref, $scope, "todos");
+        
+        $scope.addTodo = function(title) {
+            // Reset the title to an empty string
+            $scope.newTodoTitle = '';
+            // Each todo is an object with a title, completed status and a generated ID
+            // Push the newly created todo into the list
+            $scope.todos.push({
+                title: title,
+                completed: false,
+                id: generateID()
+            });
+        };
+        
+        $scope.changeCompleted = function(todo) {
+            // Update the todo
+            var message = (todo.completed === true) ? 'Task Completed!' : 'Task Uncompleted!';
+        };
+    
+        $scope.removeCompletedItems = function() {
+            // Array to contain all uncompleted todos
+            var uncompleted_todos = [];
+            // If a todo is completed, delete it
+            $scope.todos.forEach(function(todo) {
+                if (todo.completed === true) {
+                    deleteTodo(todo.id);
+                }
+                else {
+                    uncompleted_todos.push(todo);
+                }
+            });
+            // Set todo list to list of uncompleted todos
+            $scope.todos = uncompleted_todos;
+        };
+    
+        function deleteTodo(id) {
+            // Delete function only for todo_xhr.js
+        }
+    
+        // Random ID generator
+        function generateID() {
+            var rand = Math.random();
+            $scope.todos.forEach(function(todo) {
+                if (todo.id === rand) {
+                    generateID();
+                }
+            });
+            return rand;
+        }
+    }
+]);
